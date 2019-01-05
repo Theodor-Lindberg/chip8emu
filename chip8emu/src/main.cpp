3d68e9cd47f58ab065c43f4b698a36b1a0983b07
Theodor Lindberg <theodorlindberg.se@gmail.com>
2019-01-05 19:25:07 +0100

    Set up project with SDL2 in Visual Studio

---
+++ chip8emu/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "SDL.h"
+
+int main(int argc, char *argv[]) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		std::cout << "SDL could not be initialized! SDL Error: " << SDL_GetError() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
