dce9eca94903043f3f048b72a01e39cfbf95e3fc
Theodor Lindberg <theodorlindberg.se@gmail.com>
2019-01-06 13:45:29 +0100

    Started on the structure of the CPU

---
+++ chip8emu/src/chip8.cpp
@@ -0,0 +1,21 @@
+#include "chip8.h"
+
+Chip8::Chip8() { }
+
+Chip8::~Chip8() { }
+
+void Chip8::reset() {
+
+}
+
+void Chip8::init() {
+
+}
+
+bool Chip8::load_rom(const uint8_t * rom) {
+	return false;
+}
+
+void Chip8::emulate_cycle() {
+
+}
--- chip8emu/src/main.cpp
+++ chip8emu/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "SDL.h"
+#include "chip8.h"
 
 int main(int argc, char *argv[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
